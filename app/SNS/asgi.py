86e595aff74c06b1f062b1658e5703ab65f777aa
Ivan Kim <xormrdlsrks2@gmail.com>
2023-09-06 22:02:26 +0900

    Revise code: path set up

--- app/SNS/asgi.py
+++ app/SNS/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SNS.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SNS.settings.staging")
 
 application = get_asgi_application()
--- app/SNS/settings/base.py
+++ app/SNS/settings/base.py
@@ -98,7 +98,6 @@ DATABASES = {
         "USER": os.getenv("POSTGRES_USER", "postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
         "HOST": os.getenv("DB_HOST", "db"),
-        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
