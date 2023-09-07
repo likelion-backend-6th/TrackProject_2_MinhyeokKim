b4bfaecb59c21f00a9dd09607ddc8bbd0cb27bfe
Ivan Kim <xormrdlsrks2@gmail.com>
2023-09-07 21:05:55 +0900

    FEAT: add models & serializer

--- app/PostListAPI/models.py
+++ app/PostListAPI/models.py
@@ -2,23 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# User can create Posts
+# Posts can be seen by everyone if it is public
+# Posts can only be seen by the user and the people they follow if it is private
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    is_private = models.BooleanField(default=False)
-    created_date = models.DateTimeField()
-    published_date = models.DateTimeField()
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
 
 
+# User can follow other users
+# Followers can see the posts of the people they follow
+# User information has followers and following
 class Follow(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    followed_user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="followed_user"
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="follower"
+    )
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following"
     )
 
     def __str__(self):
-        return str(self.user) + " " + str(self.followed_user)
+        return str(self.follower) + "follows" + str(self.following)
--- app/PostListAPI/serializers.py
+++ app/PostListAPI/serializers.py
@@ -1,2 +1,22 @@
 from rest_framework import serializers
-from .models import Post, Follow
+from PostListAPI.models import Post, Follow
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+        read_only_fields = (
+            "id",
+            "created_at",
+            "updated_at",
+        )
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Follow
+        fields = "__all__"
+
+        read_only_fields = ("id",)
