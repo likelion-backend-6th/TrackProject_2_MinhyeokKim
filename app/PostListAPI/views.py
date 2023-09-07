740aa2ba5c78a72b6723c7fdf77fd04bf594f577
Ivan Kim <xormrdlsrks2@gmail.com>
2023-09-07 21:06:03 +0900

    FEAT: add views

--- app/PostListAPI/views.py
+++ app/PostListAPI/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets, status, permissions
 
-# Create your views here.
+from PostListAPI.models import Post, Follow
+from PostListAPI.serializers import PostSerializer, FollowSerializer
+
+
+# Post CRUD
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+# Follow CRUD
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
