e0da391e5b33734005e97aaba4e85db66eed55c9
Ivan Kim <xormrdlsrks2@gmail.com>
2023-09-11 00:22:31 +0900

    UPDATE: revise > create_dummy_data

--- app/PostListAPI/management/commands/create_dummy_data.py
+++ app/PostListAPI/management/commands/create_dummy_data.py
@@ -2,45 +2,43 @@ from django.core.management import BaseCommand
 from django.contrib.auth.models import User
 from PostListAPI.models import Post, Follow
 from django_seed import Seed
+from random import sample
 
 
 class Command(BaseCommand):
-    help = "This command creates random users and posts"
+    help = "This command creates random users, posts, and follows"
 
     def handle(self, *args, **kwargs):
         seeder = Seed.seeder()
+        fake = seeder.faker
 
         # Seed users
-        seeder.add_entity(
-            User,
-            5,
-            {
-                "username": seeder.faker.user_name(),
-                "email": seeder.faker.email(),
-                "password": "1234",
-            },
-        )
-        created_users = seeder.execute()
-        created_users_pks = created_users[User]
+        user_data = []
+        for i in range(5):
+            username = fake.user_name()
+            email = fake.email()
+            password = "1234"
+            user = User.objects.create_user(
+                username=username, email=email, password=password
+            )
+            user_data.append(user)
 
         # Seed posts
-        for user_pk in created_users_pks:
-            user = User.objects.get(pk=user_pk)
-            seeder.add_entity(
-                Post,
-                3,
-                {
-                    "title": seeder.faker.sentence(nb_words=5),
-                    "content": seeder.faker.text(max_nb_chars=20),
-                    "author": user,
-                },
-            )
-        seeder.execute()
+        for user in user_data:
+            for i in range(3):
+                title = fake.sentence(nb_words=5)
+                content = fake.text(max_nb_chars=20)
+                Post.objects.create(title=title, content=content, author=user)
 
         # Seed follows
-        for i in range(len(created_users_pks) - 1):
-            follower = User.objects.get(pk=created_users_pks[i])
-            following = User.objects.get(pk=created_users_pks[i + 1])
-            Follow.objects.create(follower=follower, following=following)
+        for user in user_data:
+            followers = []
+            for other_user in user_data:
+                if other_user != user:
+                    followers.append(other_user)
+
+            selected_followers = sample(followers, 2)
+            for follower in selected_followers:
+                Follow.objects.create(follower=user, following=follower)
 
         self.stdout.write(self.style.SUCCESS("Data seeded successfully"))
