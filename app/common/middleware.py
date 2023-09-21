2b692801933c8bc797441616548f9e1cfc3a519a
Ivan Kim <xormrdlsrks2@gmail.com>
2023-09-22 01:48:07 +0900

    feat: healthcheck

---
+++ app/common/middleware.py
@@ -0,0 +1,16 @@
+from django.http import JsonResponse
+from django.conf import settings
+
+
+class HealthcheckMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if settings.VERSION == "unhealthy":
+            return JsonResponse({"status": "unhealthy"}, status=500)
+
+        if request.path == "/health/":
+            return JsonResponse({"status": "ok"})
+
+        return self.get_response(request)
